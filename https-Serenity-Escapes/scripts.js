29faf5c41f93278f848ae7846399787d76e41895
RaeDawn Long <51103916+Girl3200@users.noreply.github.com>
2019-10-27 15:26:25 -0400

    Add files via upload

---
+++ https-Serenity-Escapes/scripts.js
@@ -0,0 +1,55 @@
+// Custom slide show
+function mySlideShow() {
+    // get slide show div
+    const slides = document.getElementById("slides");
+  
+  /*
+      Use setTimeout function to add classes to the main slideshow
+      element every two seconds that shift it to the left.
+      Using CSS to animate the transition each time.
+    */
+    setTimeout(function() {
+        slides.classList.add("slideshow-shift-1");
+    }, 2000);     
+    setTimeout(function() {
+      slides.classList.add("slideshow-shift-2");
+  }, 4000);  
+  setTimeout(function() {
+      slides.classList.add("slideshow-shift-3");
+  }, 6000);  
+  setTimeout(function() {
+      slides.classList.add("slideshow-shift-4");
+  }, 8000);  
+  setTimeout(function() {
+      slides.classList.add("slideshow-shift-5");
+  }, 10000);  
+  setTimeout(function() {
+      slides.classList.add("slideshow-shift-6");
+  }, 12000);  
+  setTimeout(function() {
+      slides.classList.add("slideshow-shift-7");
+  }, 14000);  
+  setTimeout(function() {
+      slides.classList.add("slideshow-shift-8");
+  }, 16000); 
+  
+  // At the end, we remove all the classes to reset the slideshow
+  setTimeout(function() {
+      slides.classList.remove(
+        "slideshow-shift-1", 
+        "slideshow-shift-2",
+        "slideshow-shift-3" ,
+        "slideshow-shift-4", 
+        "slideshow-shift-5",
+        "slideshow-shift-6", 
+        "slideshow-shift-7", 
+        "slideshow-shift-8"
+    );
+  // And then call it again to restart it
+    mySlideShow();
+  }, 16000);
+  // Close main mySlideShow function
+  }
+  // Start custom slide show on page load
+  mySlideShow();
+  
